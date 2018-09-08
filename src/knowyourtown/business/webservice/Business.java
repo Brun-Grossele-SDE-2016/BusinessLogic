86f336b24d64e0b87325fe99a30117e40880528a
MatteoGrossele <matteogross@live.it>
2018-09-08 22:25:48 +0200

    test heroku 3

--- src/knowyourtown/business/webservice/Business.java
+++ src/knowyourtown/business/webservice/Business.java
@@ -13,7 +13,7 @@ import javax.jws.WebService;
 import javax.jws.WebResult;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
-import javax.jws.soap.SOAPBinding.Use; 
+import javax.jws.soap.SOAPBinding.Use;
 
 @WebService
 @SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL) //optional
